c05a91f8901758c4d0f2f70bcc7f7e7ec720102f
Saka3344 <100471123+Saka3344@users.noreply.github.com>
2022-02-28 23:35:08 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -1,71 +1,47 @@
-from importlib.resources import path
 import os
 import csv
-pybank_csv=os.path.join("..","Resources","budget_data.csv")
-# Create empty lists to iterate through specific rows for the following variables
-total_months = []
-total_profit = []
-monthly_profit_change = []
- 
-# Open csv in default read mode with context manager
-with open(pybank_csv,'r') as csvfile:
 
-     # Store the contents of budget_data.csv in the variable csvreader
-    csvreader = csv.reader(csvfile,delimiter=",") 
+pollData_csv = os.path.join("..","Resources","election_data.csv")
+#currentDirectory = os.getcwd()
+#print(currentDirectory)
+#print(pollData)
 
-    # Skip the header labels to iterate with the values
-    header = next(csvreader)  
+# Open the CSV file
+import csv
 
-    # Iterate through the rows in the stored file contents
-    for row in csvreader: 
-
-        # Append the total months and total profit to their corresponding lists
-        total_months.append(row[0])
-        total_profit.append(int(row[1]))
-
-    # Iterate through the profits in order to get the monthly change in profits
-    for i in range(len(total_profit)-1):
-        
-        # Take the difference between two months and append to monthly profit change
-        monthly_profit_change.append(total_profit[i+1]-total_profit[i])
-        
-# Obtain the max and min of the the montly profit change list
-max_increase_value = max(monthly_profit_change)
-max_decrease_value = min(monthly_profit_change)
-
-# Correlate max and min to the proper month using month list and index from max and min
-#We use the plus 1 at the end since month associated with change is the + 1 month or next month
-max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
-max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
-
-# Print Statements
-
-print("Financial Analysis")
-print("----------------------------")
-print(f"Total Months: {len(total_months)}")
-print(f"Total: ${sum(total_profit)}")
-print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
-print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
-print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+# dictionary with candidates as keys and votes as values
+candidates = dict()
+count=[]
 
 
-# Output files
+# read the data file
+with open(pollData_csv, 'r') as csv_file:
+    elections_data = csv.reader(csv_file, delimiter=',')
+    next(elections_data) # skip the header
+    for row in elections_data: # iterate over all rows
+       candidate = row[2] # candidate name
+       # if candidate name is not in the dictionary, update the dictionary
+       if candidate not in candidates.keys():
+            candidates.update({candidate:0})
+       # increment the number of votes by one
+       candidates[candidate] += 1
+
+      
+    total_votes = sum(candidates.values())
+    winner = max(candidates, key=lambda k: candidates[k])
+    print("Election Results")
+    print("----------------------------")
+    print(f"Total Votes: {total_votes:,}")
+    print(f"winner:, {winner}")
 
 
-with open("Financial_Analysis_Summary.txt","w") as file:
-    
-# Write methods to print to Financial_Analysis_Summary 
-    file.write("Financial Analysis")
-    file.write("\n")
-    file.write("----------------------------")
-    file.write("\n")
-    file.write(f"Total Months: {len(total_months)}")
-    file.write("\n")
-    file.write(f"Total: ${sum(total_profit)}")
-    file.write("\n")
-    file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
-    file.write("\n")
-    file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
-    file.write("\n")
-    file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+    print("----------------------------")
 
+# write results
+    file_to_output = "resultsPoll.txt"
+    with open(file_to_output, "w") as txt_file:
+      txt_file.write(".....................................\n")
+      txt_file.write(f"Total Votes: {total_votes:,}\n")
+      txt_file.write(f"Winner: {winner} \n")
+      txt_file.write(".....................................")
+      
